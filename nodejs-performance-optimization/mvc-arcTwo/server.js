2f8be393d3dfb016922ee9944342713cd240508d
Azizullah <azizullahcodes@gmail.com>
2026-08-25 22:40:49 +0500

    upd

--- nodejs-performance-optimization/mvc-arcTwo/server.js
+++ nodejs-performance-optimization/mvc-arcTwo/server.js
@@ -3,6 +3,9 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import compression from 'compression';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
   import * as dns from "dns"; // For resolving hostnames...!
 import connectDB from "./src/db/db.js";
 
@@ -12,12 +15,30 @@ import userRoutes from "./src/routes/user-routes/user-routes.js";
   dns.setServers(["1.1.1.1", "8.8.8.8"]); // For setting custom DNS servers...!
 
 const port = 5050;
+
+// const limit = rateLimit({
+//     windowMs: 1000 * 60 * 15,
+//     max: 10,
+//     standardHeaders: true
+// });
+const limit = rateLimit({
+   windowMs : 1000 * 60 * 15,
+  max : 5,
+  standardHeaders : true
+
+})
 const server = express();
 
 server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
+server.use(helmet());
+server.use(compression());
+server.use(limit);
 server.use(userRoutes);
+
+// limit for specidfic route
+// server.use(limit, productRoutes)
 
 server.listen(port, () => {
     console.log('Your Node JS server is running!');
--- nodejs-performance-optimization/mvc-arcTwo/src/controllers/user-controller/user-controller.js
+++ nodejs-performance-optimization/mvc-arcTwo/src/controllers/user-controller/user-controller.js
@@ -89,43 +89,7 @@ catch(error){
 }
 }
 
-//node js performance optimization part start 
-// fetch user by id controller...!
-// const fetchUserByID = async (req, res) => {
-//     try {
-//         const redisKey = req.params.uid;
-//         // console.log('User id:', redisKey);
-
-//         const cachedData = cacheClient.get(redisKey);
-
-//         if (cachedData) {
-//             console.log('User fetched from server memory!');
-//             return res.status(200).send({
-//                 status: true,
-//                 message: "User fetched",
-//                 data: JSON.parse(cachedData)
-//             });
-//         };
-
-//         const fetchData = await UserModal.findById(redisKey).select('-password');
-//         cacheClient.set(redisKey, JSON.stringify(fetchData)); // Saved data in server memory...!
-//         console.log('User fetched from DB');
-
-//         return res.status(200).send({
-//             status: true,
-//             message: "User fetched",
-//             data: fetchData
-//         });
-//     }
-
-//     catch (error) {
-//         console.log(`Err while fetching user by id: ${error}`);
-//         return res.status(500).send({
-//             status: false,
-//             message: "Err while fetching user by id!"
-//         });
-//     };
-// };
+// fetchUser by Id 
 
 const fetchUserByID = async(req,res)=>{
     try{
