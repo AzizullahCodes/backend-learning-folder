76c87e9d0aee941b5f62674450811b638e2403f5
Azizullah <azizullahcodes@gmail.com>
2026-09-12 09:28:37 +0500

    upd

--- postSql_database/postSql_database_backend/server.js
+++ postSql_database/postSql_database_backend/server.js
@@ -10,6 +10,35 @@ server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
 
+//api for add user 
+server.post('/user/add', async(req,res)=>{
+    const {username,email,age} = req.body
+
+    try{
+        let addQuery = "INSERT INTO users(username,email,age) VALUES($1,$2,$3) RETURNING *"
+        const apiRes = await pool.query(
+            addQuery,
+            [username,email,age]
+        )
+        if(apiRes?.rows){
+            return res.status(200).send({
+                status : true,
+                message : 'user added successfully',
+                data : apiRes?.rows[0]
+            })
+        }
+        console.log(apiRes?.rows)
+
+    }
+    catch(error){
+        console.log('Error while adding user',error);
+        return res.status(500).send({
+            sttatus : false,
+            message : 'Internal server error'
+        })
+    }
+})
+
 server.listen(port,()=>{
     console.log('node js server is running ',port)
 })
