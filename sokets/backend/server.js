d7468d04755b64b595bb9ed379f7c4e0d987df72
Azizullah <azizullahcodes@gmail.com>
2026-09-19 16:48:18 +0500

    upd

--- sokets/backend/server.js
+++ sokets/backend/server.js
@@ -1,34 +1,78 @@
+// import express from "express";
+// import cors from "cors";
+// import morgan from "morgan";
+// import http from "http";
+// import { Server } from "socket.io";
+
+// const app = express();
+// const httpServer = http.createServer(app);
+// const PORT =  5050;
+
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   },
+// });
+
+// app.use(cors());
+// app.use(morgan("dev"));
+// app.use(express.json());
+
+// io.on("connection", (socket) => {
+  
+//   socket.on("msgFromFrontend", (data) => {
+//     console.log("message aaya:", data);
+
+//     io.emit("msgFromFromBackend", 'i am from backend'); // sabko wapas bhejo
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected", socket.id);
+//   });
+// });
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello world</h1>");
+// });
+
+// httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import http from 'http';
-import { Server } from "http";
-import { Socket } from "socket.io";
-
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-
 const httpServer = http.createServer(app);
-const PORT = process.env.PORT || 5000;
-const io = new Server(httpServer,{
-    cors : {
-        origin : '*',
-        method : ['GET','POST','PUT','DELETE']
-    }
-})
+const PORT =  5050;
 
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-//sockets functionality 
-io.on("connect",(socket)=>{
-    console.log('a user connected', socket.id)
-})
+io.on("connection", (socket) => {
+  
+  socket.on("msgFromFrontend", (data) => {
+    console.log("message aaya:", data);
 
+    io.emit("msgFromBackend", data); // sabko wapas bhejo
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
 app.get("/", (req, res) => {
-  res.send('<h1>Hello world </h1>');
+  res.send("<h1>Hello world</h1>");
 });
 
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- sokets/frontend/project-one/soket-integration/src/App.jsx
+++ sokets/frontend/project-one/soket-integration/src/App.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { io } from 'socket.io-client';
-// const socket = io('http://localhost:5050', { autoConnect: false });
-const socket = io('http://localhost:5000/',{
-  autoConnect : false
-})
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
+const socket = io("http://localhost:5050");
 
 const App = () => {
+  const [sendMessage,setSendMessage] = useState('')
+  
 
-  const submit = ()=>{
-    console.log('button clicked')
-    socket.emit("connect", "hello testing 1234")
+  const submit = () => {
+    // console.log(sendMessage)
+    socket.emit("msgFromFrontend", sendMessage)
+    // socket.emit("msgFromFrontend", "hello i am from frontend");
+  };
+
+
+  useEffect(() => {
+  const onConnect = () => {
+    console.log('sockent connected...', socket.id)
   }
 
-  useEffect(()=>{
-socket.connect();
-socket.on('connect', ()=>{
-  console.log('frontend socket connected....', socket.id)
-})
-  },[])
+  const onMsg = (msg) => {
+    console.log(msg)
+  }
+
+  socket.on('connect', onConnect)
+  socket.on("msgFromBackend", onMsg)
+
+  return () => {
+    socket.off('connect', onConnect)
+    socket.off("msgFromBackend", onMsg)
+  }
+}, []);
   return (
     <div>
-      <h1>socket learing </h1>
+      <h1>socket learning</h1>
+      <input type="text"
+      placeholder="enter message" 
+      value={sendMessage}
+      onChange={(e)=>setSendMessage(e.target.value)}
+      autoComplete="new-sendMessage"/><br/>
       <button onClick={submit}>submit</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
