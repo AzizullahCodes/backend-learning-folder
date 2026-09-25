7bfd66e367361b18b003287e51ad3f702c0d15f0
Azizullah <azizullahcodes@gmail.com>
2026-09-25 20:07:49 +0500

    upd

--- sokets/sockets-second-folder/backend/server.js
+++ sokets/sockets-second-folder/backend/server.js
@@ -1,10 +1,9 @@
-//server.js
+// server.js
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
-
 
 const app = express();
 app.use(express.json());
@@ -16,21 +15,31 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETER"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 const users = new Map();
 
 io.on("connect", (socket) => {
   console.log("socket connected successfully....", socket.id);
-//registering user name from frontend comes 
-socket.on('register',(uid)=>{
-    console.log(`user id....${uid}`)
-    users.set(uid,socket.id.)
-   console.log('users array is...',users)
 
-})
+  // registering user name from frontend comes
+  socket.on("register", (uid) => {
+    console.log(`user id....${uid}`);
+    users.set(uid, socket.id);
+    console.log("users array is...", users);
+  });
 
+  // after disconnecting
+  socket.on("disconnect", () => {
+    for (const [uid, id] of users) {
+      if (id === socket.id) {
+        users.delete(uid); // disconnect hote hi address book se naam hata do
+        console.log(`user disconnected: ${uid}`);
+        break;
+      }
+    }
+  });
 });
 
 httpServer.listen(port, () => {
