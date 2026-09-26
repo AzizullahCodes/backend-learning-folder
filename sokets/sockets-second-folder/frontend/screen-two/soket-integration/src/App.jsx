a9677d09f101c81cfb41e7fc979ff25715f773e7
Azizullah <azizullahcodes@gmail.com>
2026-09-26 12:59:17 +0500

    upd

--- sokets/sockets-second-folder/frontend/screen-two/soket-integration/src/App.jsx
+++ sokets/sockets-second-folder/frontend/screen-two/soket-integration/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:5052",{autoConnect : false});
+const socket = io("http://localhost:5050",{autoConnect : false});
 
 const App = ()=>{
   const [input,setInput] = useState('');
